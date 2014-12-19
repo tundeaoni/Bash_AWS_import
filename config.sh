00eeda54c6fe54fd72841a558cdc41a0f79f7b3f
Olatunde Alex-Oni <tunde.alexoni@yahoo.com>
2014-12-19 16:48:22 +0100

    initial push

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# first augument via terminal is the s3 bucket prefix eg clouds_
+cloud_prefix=$1
+# second augument via terminal is PATH/TO/Manifest
+manifest_file_name=$2
+#AWS accessKeyId
+aws_key="AWS Key"
+#array of drive ids e.g drives=("Bob" "Peter" "Big Bad John")
+#drives=("tunde" "alexoni" "ifeatu" "bliss501" "Onyin" "Solomon")
+
---
+++ drives.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#array of drive ids e.g drives=("Bob" "Peter" "Big Bad John")
+drives=("tunde" "alexoni" "ifeatu" "bliss501" "Onyin" "Solomon")
+
